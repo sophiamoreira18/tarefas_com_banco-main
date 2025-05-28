6f043951c3ba8caa5123103e55b5c31a809f237f
Sophia <sophia.moreira@alunos.sc.senac.br>
2025-05-28 16:11:09 -0300

    6789

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+use App\Models\Tarefa;
+use App\Database\Mariadb;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -6,10 +8,13 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
+$banco = new Mariadb();
 
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+$app->get('/usuario/{id}/tarefas', function (Request $request, Response $response, array $args) use ($banco) {
+    $user_id = (int)$args['id'];
+    $tarefa = new Tarefa($banco->getConnection());
+    $tarefas = $tarefa->getAllByUser($user_id);
+    $response->getBody()->write(json_encode($tarefas));
     return $response;
 });
 
--- src/Models/Tarefa.php
+++ src/Models/Tarefa.php
@@ -3,12 +3,67 @@ namespace App\Models;
 
 class Tarefa
 {
+  private \PDO $connection;
+  // Atributos da classe Tarefa
 
   private ?int $id = null;
   private string $titulo = "";
   private string $descricao = "";
   private bool $status = false;
   private int $user_id = 0;
-  
+
+  public function __construct(\PDO $connection)
+  {
+    $this->connection = $connection;
+  }
+
+  public function create(): bool
+  {
+    $sql = "INSERT INTO tarefas (titulo, descricao, status, user_id) VALUES (:titulo, :descricao, :status, :user_id)";
+    $stmt = $this->connection->prepare($sql);
+    return $stmt->execute([
+      ':titulo' => $this->titulo,
+      ':descricao' => $this->descricao,
+      ':status' => $this->status,
+      ':user_id' => $this->user_id
+    ]);
+  }
+
+  public function getTarefaById(int $id): ?array
+  {
+    $sql = "SELECT * FROM tarefas WHERE id = :id";
+    $stmt = $this->connection->prepare($sql);
+    $stmt->execute([':id' => $id]);
+    return $stmt->fetch();
+  }
+
+  public function update(): bool
+  {
+    $sql = "UPDATE tarefas SET titulo = :titulo, descricao = :descricao, status = :status, user_id = :user_id WHERE id = :id";
+    $stmt = $this->connection->prepare($sql);
+    return $stmt->execute([
+      ':id' => $this->id,
+      ':titulo' => $this->titulo,
+      ':descricao' => $this->descricao,
+      ':status' => $this->status,
+      ':user_id' => $this->user_id
+    ]);
+  }
+   
+  public function delete(int $id): bool
+  {
+    $sql = "DELETE FROM tarefas WHERE id = :id";
+    $stmt = $this->connection->prepare($sql);
+    return $stmt->execute([':id' => $id]);
+  }
+
+  public function getAllByUser(int $user_id) : array
+  {
+    $sql = "SELECT * FROM tarefas WHERE user_id = :user_id";
+    $stmt = $this->connection->prepare($sql);
+    $stmt->execute([':user_id' => $user_id]);
+    return $stmt->fetchAll();
+
+  }
 } 
 ?>
